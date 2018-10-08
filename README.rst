c839d922f6a152ee6a4e2a843156020d85ab6b58
Alexander Artemenko <svetlyak.40wt@gmail.com>
2018-10-08 09:01:55 +0300

    Script is configurable now.

---
+++ README.rst
@@ -0,0 +1,6 @@
+Configurable parameters
+=======================
+
+* ``BUILD_DOCS_FROM_BRANCH`` (default - "master")
+* ``BUILD_DOCS_WHEN_LISP`` (default - "ccl")
+* ``BUILD_DOCS_WHEN_OS`` (default - "linux")
--- build-docs.sh
+++ build-docs.sh
@@ -2,13 +2,20 @@
 
 set -e
 
+BUILD_DOCS_FROM_BRANCH=${BUILD_DOCS_FROM_BRANCH:-master}
+BUILD_DOCS_WHEN_LISP=${BUILD_DOCS_WHEN_LISP:-ccl}
+BUILD_DOCS_WHEN_OS=${BUILD_DOCS_WHEN_OS:-linux}
+
 # we only want to build documentation from the "reblocks" branch
 # and want to make it only once.
 # Also, we need to check if TRAVIS_PULL_REQUEST=false because Travis
 # builds which are running for pull requests will have
 # TRAVIS_BRANCH=reblocks, but TRAVIS_PULL_REQUEST_BRANCH=the-branch
 # and TRAVIS_PULL_REQUEST=42 where 42 is a pull request number
-if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" -a "$LISP" = "ccl" -a "$TRAVIS_OS_NAME" = "linux" ]; then
+if [ "$TRAVIS_BRANCH" = "$BUILD_DOCS_FROM_BRANCH" \
+  -a "$TRAVIS_PULL_REQUEST" = "false" \
+  -a "$LISP" = "$BUILD_DOCS_WHEN_LISP" \
+  -a "$TRAVIS_OS_NAME" = "$BUILD_DOCS_WHEN_OS" ]; then
     virtualenv env --python=python2.7
     source env/bin/activate
     python --version
