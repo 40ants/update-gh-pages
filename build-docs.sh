04a32c2498cd683e9ced48a1df0b280b6054d215
Alexander Artemenko <svetlyak.40wt@gmail.com>
2018-10-08 11:30:53 +0300

    Returned back.

--- build-docs.sh
+++ build-docs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 BUILD_DOCS_FROM_BRANCH=${BUILD_DOCS_FROM_BRANCH:-master}
 BUILD_DOCS_WHEN_LISP=${BUILD_DOCS_WHEN_LISP:-ccl}
@@ -12,9 +13,9 @@ BUILD_DOCS_WHEN_OS=${BUILD_DOCS_WHEN_OS:-linux}
 # builds which are running for pull requests will have
 # TRAVIS_BRANCH=reblocks, but TRAVIS_PULL_REQUEST_BRANCH=the-branch
 # and TRAVIS_PULL_REQUEST=42 where 42 is a pull request number
-if [ "$TRAVIS_BRANCH" = "$BUILD_DOCS_FROM_BRANCH"
-  -a "$TRAVIS_PULL_REQUEST" = "false"
-  -a "$LISP" = "$BUILD_DOCS_WHEN_LISP"
+if [ "$TRAVIS_BRANCH" = "$BUILD_DOCS_FROM_BRANCH" \
+  -a "$TRAVIS_PULL_REQUEST" = "false" \
+  -a "$LISP" = "$BUILD_DOCS_WHEN_LISP" \
   -a "$TRAVIS_OS_NAME" = "$BUILD_DOCS_WHEN_OS" ]; then
     virtualenv env --python=python2.7
     source env/bin/activate
